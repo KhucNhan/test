5c34494296713c0f9f4e50f37ba11514b08b4801
Khuc Nhan <khucvii@gmail.com>
2024-04-04 16:05:48 +0700

    update

--- src/OTH.java
+++
@@ -1,6 +0,0 @@
-import static java.lang.Integer.parseInt;
-public class OTH {
-        public int onesNumLength = parseInt(String.valueOf(numLength - 1));
-        public int tensNumLength = parseInt(String.valueOf(numLength - 2));
-        public int hundredsNumLength = parseInt(String.valueOf(numLength - 3));
-}
