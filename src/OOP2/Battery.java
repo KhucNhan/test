f96057d519385496103e39a4b733a431d97ab304
Khuc Nhan <khucvii@gmail.com>
2024-04-17 14:07:55 +0700

    update

--- src/OOP2/Battery.java
+++ src/OOP2/Battery.java
@@ -1,4 +1,59 @@
 package OOP2;
-public class Battery {
+public class Battery{
+    int energy;
+    Battery(int energy) {
+        this.energy = energy;
+    }
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public void getEnergy() {
+        System.out.println(this.energy);
+    }
+
+    public void decreaseEnergy() {
+        this.energy -= 1;
+    }
+}
+class Lamp {
+    boolean status;
+    Battery battery;
+    public void setBattery(Battery battery) {
+        this.battery = battery;
+    }
+
+    public void turnOn() {
+        this.status = true;
+    }
+
+    public void turnOff() {
+        this.status = false;
+    }
+    public void getStatus() {
+        System.out.println(this.status);
+    }
+
+    public int getBatteryInfor() {
+        return battery.energy;
+    }
+
+    public void light() {
+        if (this.status) {
+            System.out.println("Light on.");
+        } else {
+            System.out.println("Light off.");
+        }
+    }
+    public static void main(String[] args) {
+        Battery pin1 = new Battery(100);
+        Lamp den1 = new Lamp();
+        den1.setBattery(pin1);
+        System.out.println(den1.getBatteryInfor());
+        den1.turnOn();
+        den1.getStatus();
+        den1.light();
+        System.out.println(den1.battery);
+    }
 
 }
--- src/OOP2/Tom_And_Jerry.java
+++ src/OOP2/Tom_And_Jerry.java
@@ -24,3 +24,59 @@ public class Tom_And_Jerry {
         }
     }
 }
+
+class Rat {
+    public String name;
+    public double weight;
+    public int speed;
+    public Boolean condition;
+    Rat(String name, double weight, int speed, Boolean condition) {
+        this.name = name;
+        this.weight = weight;
+        this.speed = speed;
+        this.condition = condition;
+    }
+
+    public void sound() {
+        System.out.println("Chit chit chit");
+    }
+
+    public void name() {
+        System.out.println(this.name);
+    }
+
+    public void speed() {
+        System.out.println(this.speed);
+    }
+
+    public void weight() {
+        System.out.println(this.weight);
+    }
+}
+
+class Cat {
+    public String name;
+    public double weight;
+    public int speed;
+    Cat(String name, double weight, int speed) {
+        this.name = name;
+        this.weight = weight;
+        this.speed = speed;
+    }
+
+    public void sound() {
+        System.out.println("Meow meow");
+    }
+
+    public void name() {
+        System.out.println(this.name);
+    }
+
+    public void speed() {
+        System.out.println(this.speed);
+    }
+
+    public void weight() {
+        System.out.println(this.weight);
+    }
+}
