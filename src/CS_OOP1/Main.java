8d13ce7be15d99466cc61152da729b632151bcf7
Khuc Nhan <khucvii@gmail.com>
2024-05-28 14:27:08 +0700

    update Case Study OOP1 - LibraryConsole

--- src/CS_OOP1/Main.java
+++ src/CS_OOP1/Main.java
@@ -1,196 +1,32 @@
 package CS_OOP1;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Library admin = new Library();
-        admin.add(new Ebook("01","a","m","2000","Animation","123 MB","pdf"));
-        admin.add(new Ebook("02","b","m","1995","Fantasy","1 GB","pdf"));
-        admin.add(new Ebook("03","c","n","1999","Animation","456 MB","pdf"));
-        do {
-            System.out.println("---------------------------------------");
-            System.out.println("1. Add new ebook to library.");
-            System.out.println("2. Delete ebook from library.");
-            System.out.println("3. Update ebook information.");
-            System.out.println("4. Search ebook.");
-            System.out.println("5. Sort ebook.");
-            System.out.println("6. Show all ebook in library.");
-            System.out.println("7. Exit.");
-            int number = new Scanner(System.in).nextInt();
-            switch (number) {
+        LibraryConsole adminConsole = new LibraryConsole();
+        while (true) {
+            int choice = adminConsole.menu();
+            switch (choice) {
                 case 1:
-                    System.out.print("Book ID: ");
-                    String eBookId = new Scanner(System.in).nextLine();
-                    while (admin.isExist(eBookId)) {
-                        System.out.println("This id have been existed.");
-                        eBookId = new Scanner(System.in).nextLine();
-                    }
-                    System.out.print("Title: ");
-                    String title = new Scanner(System.in).nextLine();
-                    System.out.print("Author: ");
-                    String author = new Scanner(System.in).nextLine();
-                    System.out.print("Release year: ");
-                    String releaseYear = new Scanner(System.in).nextLine();
-                    System.out.print("Genre: ");
-                    String genre = new Scanner(System.in).nextLine();
-                    System.out.print("Size: ");
-                    String size = new Scanner(System.in).nextLine();
-                    System.out.print("Format: ");
-                    String format = new Scanner(System.in).next();
-                    admin.add(new Ebook(eBookId, title, author, releaseYear, genre, size, format));
+                    adminConsole.add();
                     break;
                 case 2:
-                    if (admin.isEmpty()) {
-                        System.out.println("The library is empty.");
-                        break;
-                    }
-                    System.out.print("Enter book id to delete: ");
-                    admin.remove(new Scanner(System.in).next());
+                    adminConsole.remove();
                     break;
                 case 3:
-                    if (admin.isEmpty()) {
-                        System.out.println("The library is empty.");
-                        break;
-                    }
-                    System.out.print("Enter ebook id you want to update information: ");
-                    String updateEbookId = new Scanner(System.in).next();
-                    if (admin.getEbook(updateEbookId) != null) {
-                        System.out.println("What information do you want to update?");
-                        System.out.print("1. ID  ");
-                        System.out.print("2. Title  ");
-                        System.out.print("3. Author  ");
-                        System.out.println("4. Release year.");
-                        System.out.print("5. Genre  ");
-                        System.out.print("6. Size  ");
-                        System.out.print("7. Format  ");
-                        System.out.println("8. All.");
-                        System.out.println("9. Cancel.");
-                        int updateNumber = new Scanner(System.in).nextInt();
-                        switch (updateNumber) {
-                            case 1:
-                                System.out.println("Enter new id: ");
-                                admin.getEbook(updateEbookId).setId(new Scanner(System.in).nextLine());
-                                break;
-                            case 2:
-                                System.out.println("Enter new title: ");
-                                admin.getEbook(updateEbookId).setTitle(new Scanner(System.in).nextLine());
-                                break;
-                            case 3:
-                                System.out.println("Enter new author: ");
-                                admin.getEbook(updateEbookId).setAuthor(new Scanner(System.in).nextLine());
-                                break;
-                            case 4:
-                                System.out.println("Enter new release year: ");
-                                admin.getEbook(updateEbookId).setRelease_year(new Scanner(System.in).next());
-                                break;
-                            case 5:
-                                System.out.println("Enter new genre: ");
-                                admin.getEbook(updateEbookId).setTitle(new Scanner(System.in).nextLine());
-                                break;
-                            case 6:
-                                System.out.println("Enter new size: ");
-                                admin.getEbook(updateEbookId).setSize(new Scanner(System.in).nextLine());
-                                break;
-                            case 7:
-                                System.out.println("Enter new format: ");
-                                admin.getEbook(updateEbookId).setTitle(new Scanner(System.in).next());
-                                break;
-                            case 8:
-                                System.out.println("Enter new id: ");
-                                String updateNewId = new Scanner(System.in).nextLine();
-                                System.out.println("Enter new title: ");
-                                String updateNewTitle = new Scanner(System.in).nextLine();
-                                System.out.println("Enter new author: ");
-                                String updateNewAuthor = new Scanner(System.in).nextLine();
-                                System.out.println("Enter new release year: ");
-                                String updateNewReleaseYear = new Scanner(System.in).next();
-                                System.out.println("Enter new genre: ");
-                                String updateNewGenre = new Scanner(System.in).nextLine();
-                                System.out.println("Enter new size: ");
-                                String updateNewSize = new Scanner(System.in).nextLine();
-                                System.out.println("Enter new format: ");
-                                String updateNewFormat = new Scanner(System.in).next();
-                                admin.getEbook(updateEbookId).setEbook(updateNewId, updateNewTitle, updateNewAuthor, updateNewReleaseYear, updateNewGenre, updateNewSize, updateNewFormat);
-                                break;
-                            case 9:
-                                break;
-                        }
-                    } else {
-                        System.out.println("Your ebook id didn't match any id.");
-                    }
+                    adminConsole.update();
                     break;
                 case 4:
-                    if (admin.isEmpty()) {
-                        System.out.println("The library is empty.");
-                        break;
-                    }
-                    System.out.println("You want to find ebook by what?");
-                    System.out.println("1. Title.");
-                    System.out.println("2. Author.");
-                    System.out.println("3. Release year.");
-                    System.out.println("4. Cancel.");
-                    int findNumber = new Scanner(System.in).nextInt();
-                    switch (findNumber) {
-                        case 1:
-                            System.out.print("Enter title: ");
-                            for (Ebook e : admin.searchEbooks(new EbookSearchByTitle(), new Scanner(System.in).nextLine())) {
-                                System.out.println(e);
-                            }
-                            break;
-                        case 2:
-                            System.out.print("Enter author: ");
-                            for (Ebook e : admin.searchEbooks(new EbookSearchByAuthor(), new Scanner(System.in).nextLine())) {
-                                System.out.println(e);
-                            }
-                            break;
-                        case 3:
-                            System.out.print("Enter release year: ");
-                            for (Ebook e : admin.searchEbooks(new EbookSearchByReleaseYear(), new Scanner(System.in).nextLine())) {
-                                System.out.println(e);
-                            }
-                            break;
-                        case 4:
-                            break;
-                    }
+                    adminConsole.search();
                     break;
                 case 5:
-                    if (admin.isEmpty()) {
-                        System.out.println("The library is empty.");
-                        break;
-                    }
-                    System.out.println("You want to sort ebook by what?");
-                    System.out.println("1. Title.");
-                    System.out.println("2. Author.");
-                    System.out.println("3. Release year.");
-                    System.out.println("4. Cancel.");
-                    int sortNumber = new Scanner(System.in).nextInt();
-                    switch (sortNumber) {
-                        case 1:
-                            admin.sort(new EbookSortByTitle());
-                            break;
-                        case 2:
-                            admin.sort(new EbookSortByAuthor());
-                            break;
-                        case 3:
-                            admin.sort(new EbookSortByReleaseYear());
-                            break;
-                        case 4:
-                            break;
-                    }
+                    adminConsole.sort();
                     break;
                 case 6:
-                    if (admin.isEmpty()) {
-                        System.out.println("The library is empty.");
-                        break;
-                    }
-                    for (int i = 0; i < admin.getBookAmount(); i++) {
-                        System.out.println((admin.display())[i]);
-                    }
+                    if (adminConsole.isEmpty()) break;
+                    adminConsole.display();
                     break;
                 case 7:
                     return;
             }
-        } while (true);
+        }
     }
 }
